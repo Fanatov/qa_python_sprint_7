59e07241718883b829270db50615defcf13675a0
Alexander Volobuev <fanatov@gmail.com>
2024-08-13 08:58:22 +0300

    changes 13.08

--- test/test_order_buttons.py
+++ test/test_order_buttons.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from data import Urls
@@ -11,18 +13,16 @@ class TestButtons:
 
     def test_big_order_button(self, driver):
         driver.get(Urls.MAIN_PAGE_URL)
-        base_button = BasePage(driver)
-        base_button.accept_cookies()
         login_page = MainPageScooter(driver)
+        login_page.accept_cookies()
         login_page.click_order_big_button()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located(OrderPageLocators.order_title))
+        login_page.wait_for_element(OrderPageLocators.order_title)
         assert login_page.driver.current_url == Urls.ORDER_URL
 
     def test_small_order_button(self, driver):
         driver.get(Urls.MAIN_PAGE_URL)
         login_page = MainPageScooter(driver)
+        login_page.accept_cookies()
         login_page.click_order_small_button()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located(OrderPageLocators.order_title))
+        login_page.wait_for_element(OrderPageLocators.order_title)
         assert login_page.driver.current_url == Urls.ORDER_URL
--- test/test_order_page.py
+++ test/test_order_page.py
@@ -15,24 +15,18 @@ class TestOrderPage:
     def test_order_field(self, name, surname, address, metro_station, phone_number, ddmmyy, days, color, comment, driver):
         driver.get(Urls.ORDER_URL)
         order_page = OrderPageScooter(driver)
-        base_page = BasePage(driver)
-        base_page.accept_cookies()
+        order_page.accept_cookies()
         order_page.fill_person_info(name, surname, address, metro_station, phone_number)
         order_page.fill_rent_info(ddmmyy, days, color, comment)
-        WebDriverWait(driver, 3).until(
-            expected_conditions.visibility_of_element_located(OrderPageLocators.order_ready_pop_up))
-        order_output = driver.find_element(*OrderPageLocators.order_ready_pop_up).text
-        order_number = order_output.split(':')[1].split('.')[0].replace(' ', '')
+        order_page.wait_for_element(OrderPageLocators.order_ready_pop_up)
+        order_number = order_page.get_order_number(OrderPageLocators.order_ready_pop_up)
         order_page.click_look_order_status()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.url_to_be(f'https://qa-scooter.praktikum-services.ru/track?t={order_number}'))
-        base_page.click_scooter_logo()
-        WebDriverWait(driver, 3).until(
-            expected_conditions.url_to_be(Urls.MAIN_PAGE_URL))
-        base_page.click_yandex_logo()
+        order_page.wait_for_url_element(data.Urls.NON_FINISHED_URL+f'{order_number}')
+        order_page.click_scooter_logo()
+        order_page.wait_for_url_element(Urls.MAIN_PAGE_URL)
+        order_page.click_yandex_logo()
         driver.switch_to.window(driver.window_handles[1])
-        WebDriverWait(driver, 10).until(
-            expected_conditions.url_to_be(Urls.REDIRECT_URL))
+        order_page.wait_for_url_element(Urls.REDIRECT_URL)
         expected_url = Urls.REDIRECT_URL
         redirect_url = driver.current_url
         driver.switch_to.window(driver.window_handles[0])
